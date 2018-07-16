c6fe3bfe30bba066db8648908b2ea877938e9e3c
Damon <ambroise1.durand@gmail.com>
2018-07-17 01:32:11 +0200

    Project Base

---
+++ The passeur/The passeur/Assets/Scripts/ShipControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour {
+
+	float speed = 10f;
+	float vectircalSpeed = 0.5f;
+	float minusVectircalSpeed =2f;
+
+
+	Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+		{
+			
+			rigidbody.AddForce(transform.up * vectircalSpeed);
+		}
+
+		if (Input.GetKey("s"))
+		{
+			rigidbody.AddForce(-rigidbody.velocity.normalized * minusVectircalSpeed);
+		}
+
+		if (Input.GetKey("a"))
+		{
+			rigidbody.AddTorque(1 * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey("d"))
+		{
+			rigidbody.AddTorque(-1 * speed * Time.deltaTime);
+		}
+
+	}
+}
---
+++ The passeur/The passeur/Assets/Scripts/lookAtMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtMouse : MonoBehaviour {
+
+	Vector3 mouse_pos;
+	Transform target ; //Assign to the object you want to rotate
+	Vector3 object_pos ;
+	float angle;
+	// Use this for initialization
+	public float RotationSpeed = 5;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = Camera.main.WorldToScreenPoint(transform.position);
+		var dir = Input.mousePosition - pos;
+		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+}
