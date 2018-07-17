55f6d9e75ddbfdcba6d164b0db2d52503382235b
Damon <ambroise1.durand@gmail.com>
2018-07-17 02:40:16 +0200

    Add lightControl and correct the playerBoat controles

--- The passeur/The passeur/Assets/Scripts/ShipControl.cs
+++ The passeur/The passeur/Assets/Scripts/ShipControl.cs
@@ -4,40 +4,49 @@ using UnityEngine;
 
 public class ShipControl : MonoBehaviour {
 
-	float speed = 10f;
-	float vectircalSpeed = 0.5f;
+	float speed = 30f;
+	float vectircalSpeed = 0f;
 	float minusVectircalSpeed =2f;
-
-
+	GameObject playerSpotLight;
+	GameObject playerPointLight;
 	Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D>();
+		playerSpotLight = GameObject.Find("playerSpotLight");
+		playerPointLight = GameObject.Find("playerPointLight");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("w"))
+		transform.Translate(new Vector3(0, 1, 0) * vectircalSpeed * Time.deltaTime);
+		if (Input.GetKey("w") && vectircalSpeed <= 5)
 		{
-			
-			rigidbody.AddForce(transform.up * vectircalSpeed);
+			vectircalSpeed += 0.05f;
 		}
 
-		if (Input.GetKey("s"))
+		if (Input.GetKey("s") && vectircalSpeed >= 0.05f)
 		{
-			rigidbody.AddForce(-rigidbody.velocity.normalized * minusVectircalSpeed);
+			vectircalSpeed -= 0.05f;
 		}
 
 		if (Input.GetKey("a"))
 		{
-			rigidbody.AddTorque(1 * speed * Time.deltaTime);
+			transform.Rotate(0, 0, Input.GetAxis("Horizontal") * speed * Time.deltaTime);
 		}
 
 		if (Input.GetKey("d"))
 		{
-			rigidbody.AddTorque(-1 * speed * Time.deltaTime);
+			transform.Rotate(0, 0, Input.GetAxis("Horizontal") * speed * Time.deltaTime);
 		}
-
+		if (Input.GetKeyDown("e"))
+		{
+			playerPointLight.GetComponent<lightControl>().onOff();
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			playerSpotLight.GetComponent<lightControl>().onOff();
+		}
 	}
 }
